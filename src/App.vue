6fc2a2609266114fe83ba300ec4d925cbd6b9ac1
franedsandi <franedsandi@gmail.com>
2023-12-06 21:51:14 +0100

    [conessione api da boolfolio a laravel api

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <script>
-
+import axios from 'axios';
+import {store} from './data/store';
 export default{
   name: 'App',
   data(){
     return {
-      title: 'projects'
+      title: 'Projects'
     }
+  },
+  methods: {
+    getApi(){
+      axios.get(store.apiUrl + 'Projects')
+      .then(results => {
+        console.log(results.data);
+      });
+    }
+  },mounted(){
+    this.getApi();
   }
 }
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <h1>{{title}}</h1>
+    <div></div>
   </div>
 </template>
 
