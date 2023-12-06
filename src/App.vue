c39b29f1e5e4f37631026c48e57e37ededd79e67
franedsandi <franedsandi@gmail.com>
2023-12-06 20:27:34 +0100

    innit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+
+export default{
+  name: 'App',
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>projects</h1>
+  </div>
+</template>
+
+<style lang="scss" >
+
+</style>
